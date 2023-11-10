084a3fb568707a05cf6dbf0506bda73e3e16d6f8
jaswanthinvally <jaswanthvenkat001@gmail.com>
2023-11-10 22:58:44 +0530

    add

---
+++ website/src/components/head/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+    <h1>hello world</h1>
+    <button>+</button>
+    <button>-</button>
+    </div>
+  )
+}
+
+export default Nav
